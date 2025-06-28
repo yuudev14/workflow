7d1e426ea0dbc1ff5945718035bd98ec6f6a6efd
yuudev14 <takaki.yu.dev@gmail.com>
2025-06-28 17:36:46 +0900

    added dockerfile.dev for connector service and frontend

--- app/workflow-frontend/src/app/layout.tsx
+++ app/workflow-frontend/src/app/layout.tsx
@@ -35,6 +35,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`} suppressHydrationWarning
       >
+       aaaasd
         <Providers>
           <SidebarProvider>
             <AppSidebar />
