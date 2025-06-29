aec2f9bcff909c4739901c1da72b4bee907e2f2a
yuudev14 <takaki.yu.dev@gmail.com>
2025-06-29 12:15:24 +0900

    setup ngix

--- app/workflow-frontend/src/settings/urls.ts
+++ app/workflow-frontend/src/settings/urls.ts
@@ -1,6 +1,8 @@
 const BASE_URL = {
-  WORKFLOW_SERVICE_API: process.env.NEXT_PUBLIC_WORKFLOW_SERVICE_API,
-  CONNECTORS_SERVICE_API: process.env.NEXT_PUBLIC_CONNECTORS_SERVICE_API
+  WORKFLOW_SERVICE_API: (process.env.NEXT_PUBLIC_WORKFLOW_SERVICE_API || "") + "/workflow-api",
+  CONNECTORS_SERVICE_API: (process.env.NEXT_PUBLIC_CONNECTORS_SERVICE_API || "") + "/connector-api",
 }
+
+console.log(BASE_URL)
 
 export default BASE_URL
