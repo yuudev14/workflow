4c594810eec451490f05942160eb5b5f74aa94dd
yuudev14 <takaki.yu.dev@gmail.com>
2025-07-11 23:55:39 +0900

    fix workflow list page

--- app/workflow-frontend/src/app/workflows/(view)/page.tsx
+++ app/workflow-frontend/src/app/workflows/(view)/page.tsx
@@ -4,6 +4,15 @@ import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import WorkflowService from "@/services/worfklows/workflows";
 import Link from "next/link";
+import { Skeleton } from "@/components/ui/skeleton";
+
+const WorkflowListLoader = () => {
+  return [...Array(5)].map((_, _i) => (
+    <li key={`workflow-list-skeleton-${_i}`}>
+      <Skeleton className="h-[81.33px] rounded-sm"></Skeleton>
+    </li>
+  ));
+};
 
 export default function Page() {
   const workflowQuery = useQuery({
@@ -13,24 +22,24 @@ export default function Page() {
     },
   });
 
-  if (workflowQuery.data === undefined) {
-    return <></>;
-  }
-
   return (
     <div className="flex h-full py-3">
       <ul className="flex flex-col w-full gap-4">
-        {workflowQuery.data.entries.map((workflow) => (
-          <Link
-            href={"/workflows/" + workflow.id}
-            key={`playbook-${workflow.id}`}
-            className="flex flex-col items-start w-full gap-2 p-4 text-sm leading-tight border rounded-sm whitespace-nowrap dark:bg-muted/50 hover:bg-muted/30 hover:text-sidebar-accent-foreground">
-            <div className="flex flex-col w-full">
-              <p className="text-lg font-medium">{workflow.name}</p>
-              <p className="text-xs">active</p>
-            </div>
-          </Link>
-        ))}
+        {workflowQuery.isLoading ? (
+          <WorkflowListLoader />
+        ) : (
+          workflowQuery.data?.entries?.map((workflow) => (
+            <Link
+              href={"/workflows/" + workflow.id}
+              key={`playbook-${workflow.id}`}
+              className="flex flex-col items-start w-full gap-2 p-4 text-sm leading-tight border rounded-sm whitespace-nowrap dark:bg-muted/50 hover:bg-muted/30 hover:text-sidebar-accent-foreground">
+              <div className="flex flex-col w-full">
+                <p className="text-lg font-medium">{workflow.name}</p>
+                <p className="text-xs">active</p>
+              </div>
+            </Link>
+          ))
+        )}
       </ul>
     </div>
   );
