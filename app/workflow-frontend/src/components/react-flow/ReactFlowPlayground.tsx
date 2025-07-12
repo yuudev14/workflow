f7c0e5adc735bb3e4750d403c5ab3c178ae9ee38
yuudev14 <takaki.yu.dev@gmail.com>
2025-07-12 11:38:13 +0900

    change to black background

--- app/workflow-frontend/src/components/react-flow/ReactFlowPlayground.tsx
+++ app/workflow-frontend/src/components/react-flow/ReactFlowPlayground.tsx
@@ -1,39 +1,60 @@
-import { ReactFlow, Controls, Background, ReactFlowProps, Node, Edge, MiniMap } from '@xyflow/react';
-import '@xyflow/react/dist/style.css';
-import { useTheme } from 'next-themes';
-import { useMemo } from 'react';
-import PlaybookNode from './PlaybookNode';
+import {
+  ReactFlow,
+  Controls,
+  Background,
+  ReactFlowProps,
+  Node,
+  Edge,
+  MiniMap,
+  BackgroundVariant,
+} from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
+import { useTheme } from "next-themes";
+import { useMemo } from "react";
+import PlaybookNode from "./PlaybookNode";
 
 const nodeTypes = {
-  playbookNodes: PlaybookNode
-}
+  playbookNodes: PlaybookNode,
+};
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const ReactFlowPlayground = <T extends Record<string, any>>({ flowProps }: {
-  flowProps: ReactFlowProps<Node<T>, Edge>
+const ReactFlowPlayground = <T extends Record<string, any>>({
+  flowProps,
+}: {
+  flowProps: ReactFlowProps<Node<T>, Edge>;
 }) => {
-  const { theme } = useTheme()
+  const { theme } = useTheme();
 
   const reactFlowTheme = useMemo(() => {
-    return ["dark", "light", "system", undefined].includes(theme) ? theme as "dark" | "light" | "system" | undefined : "system"
-  }, [theme])
+    return ["dark", "light", "system", undefined].includes(theme)
+      ? (theme as "dark" | "light" | "system" | undefined)
+      : "system";
+  }, [theme]);
+
+  function resolveBgColor(mode: "dark" | "light" | "system" | undefined) {
+    if (mode === "dark") return "black";
+    if (mode === "light") return "white";
+    if (mode === "system") {
+      return window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "black"
+        : "white";
+    }
+    return "white";
+  }
 
   return (
-    <div className='h-full w-full'>
+    <div className="h-full w-full">
       <ReactFlow
         colorMode={reactFlowTheme}
         nodeTypes={nodeTypes}
         zoomOnDoubleClick={false}
-
-        {...flowProps}
-
-      >
+        {...flowProps}>
         <MiniMap />
-        <Background />
+        <Background bgColor={resolveBgColor(reactFlowTheme)} />
         <Controls />
       </ReactFlow>
     </div>
   );
-}
+};
 
-export default ReactFlowPlayground
+export default ReactFlowPlayground;
