d9ff25ac43d30354532b4e1a1324ecdcd15ff74e
yuudev14 <takaki.yu.dev@gmail.com>
2025-09-27 13:33:33 +0900

    added refactor

--- app/workflow-frontend/src/components/react-flow/nodes/TaskHistoryNode.tsx
+++ app/workflow-frontend/src/components/react-flow/nodes/TaskHistoryNode.tsx
@@ -7,19 +7,19 @@ import { Avatar, AvatarFallback } from "../../ui/avatar";
 import { TaskHistory } from "@/services/worfklows/workflows.schema";
 import { FLOW_SELECT_TRIGGER_ID } from "@/settings/reactFlowIds";
 import RenderHandles from "./RenderHandles";
-import DeleteNodeButton from "./DeleteNodeButton";
 import { useMemo } from "react";
-import { borderStatusIndicator } from "@/lib/utils";
 
 
 const TaskHistoryNode: React.FC<NodeProps<Node<TaskHistory>>> = (props) => {
 
-  const statusIndicator = useMemo(() => {
-    return borderStatusIndicator(props.data.status)
+  const borderStatusIndicator = useMemo(() => {
+    if (props.data.status === "in_progress") return "border-yellow-700"
+    if (props.data.status === "success") return "border-green-700"
+    if (props.data.status === "failed") return "border-red-700"
   }, [props.data.status])
 
   return (
-    <div className={`p-3 border-2 ${statusIndicator} group rounded-[7px]`}>
+    <div className={`p-3 border-2 ${borderStatusIndicator} group rounded-[7px]`}>
       {/* Node Content */}
       <div className="flex items-center w-full gap-3">
         <Avatar className="size-10">
